b73cb2765829685eb72ca4669c71f35ab7dcb14c
Syahda Romansyah <syahdarom@gmail.com>
2023-05-02 00:30:43 +0700

    chore(project): initialize project setup

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'unused-imports'],
+  root: true,
+  rules: {
+    '@typescript-eslint/no-misused-promises': [
+      2,
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# Happy e-Commerce Web Admin
