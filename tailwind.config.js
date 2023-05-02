c7a9f6d05a47c2f9ddb92daa07d41ddb9bc79878
Syahda Romansyah <syahdarom@gmail.com>
2023-05-02 08:53:53 +0700

    chore(config): add Inter and Poppins fonts into Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const twDefaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['var(--font-poppins)', ...twDefaultTheme.fontFamily.sans],
+        inter: ['var(--font-inter)', ...twDefaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
