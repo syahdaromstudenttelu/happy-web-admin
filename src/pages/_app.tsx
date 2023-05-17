68adb1d87e801d7ae6acab703a3a1198bb2fe069
Syahda Romansyah <syahdarom@gmail.com>
2023-05-17 19:56:08 +0700

    feat: update redux store initialization on _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import LayoutPage from '@/components/LayoutPage';
-import { reduxStore } from '@/redux-app/store/redux-store';
+import { setupStore } from '@/redux-app/store/redux-store';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Provider as ReduxProvider } from 'react-redux';
+
+const reduxStore = setupStore();
 
 export default function CustomApp({ Component, pageProps }: AppProps) {
   return (
